4ee639459e7b396f1e82ec9dae6bc1d8b71c3985
raresNagy <rares.nagy@gmail.com>
2022-11-23 22:46:53 +0200

    adaugat frana de motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -8,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class RobotHardware {
 
-    private OpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+    private final OpMode myOpMode;   // gain access to methods in the calling OpMode.
 
     // Define a constructor that allows the OpMode to pass a reference to itself.
 
@@ -35,14 +34,27 @@ public class RobotHardware {
         backRight = myOpMode.hardwareMap.get(DcMotor.class, "MotorBackRight");
         fourbar1 = myOpMode.hardwareMap.get(DcMotor.class, "MotorFourbar");
 
+        fourbar1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fourbar2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         fourbar1.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void movement(Gamepad gamepad1) {
-        double frontLeftPower = +gamepad1.left_stick_y -
+        double frontLeftPower = gamepad1.left_stick_y -
                 gamepad1.left_stick_x -
                 gamepad1.right_stick_x;
-        double backLeftPower = +gamepad1.left_stick_y +
+        double backLeftPower = gamepad1.left_stick_y +
                 gamepad1.left_stick_x -
                 gamepad1.right_stick_x;
         double frontRightPower = -gamepad1.left_stick_y -
