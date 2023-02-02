441c9206167744bc81b2e6f45ac4ebf35a897641
raresNagy <rares.nagy@gmail.com>
2023-02-02 15:45:35 +0200

    pozitii vfb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Prototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Prototype.java
@@ -8,34 +8,39 @@ import org.firstinspires.ftc.teamcode.utilities.PIDController;
 
 @TeleOp(name = "prototype", group = "B")
 public class Prototype extends OpMode {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	static final int RIDICARE_POS_1 = 5100;
 	static final int RIDICARE_POS_2 = 9800;
 	static final int RIDICARE_POS_3 = 16500;
 	static final double VFB_OUTTAKE_POS = 0.3;
-	static final double VFB_INTAKE_POS = 0;
+	static final double VFB_INTAKE_POS = 0.05;
+
+	static final double VFB_POS3=0.8;
+
+	static final double VFB_POS4=0.65;
 
 	RobotHardware robot = new RobotHardware(this);
 	PIDController ridicareController = new PIDController(0.012, 0.25, 0.000144, this);
 	double ridicareTarget = 0;
-=======
-    static final int RIDICARE_POS_1 = 14278;
-    static final int RIDICARE_POS_2 = 30000;
-    static final int RIDICARE_POS_3 = 55000;
-    static final double VFB_OUTTAKE_POS = 0.3;
-    static final double VFB_INTAKE_POS = 0;
 
-    RobotHardware robot = new RobotHardware(this);
-    PIDController ridicareController = new PIDController(0.004, 0.008, 1, this);
-    double ridicareTarget = 1;
->>>>>>> 9340429984637aa233529f867469ddf65201ed7f
+//=======
+    //static final int RIDICARE_POS_1 = 14278;
+    //static final int RIDICARE_POS_2 = 30000;
+    //static final int RIDICARE_POS_3 = 55000;
+  //  static final double VFB_OUTTAKE_POS = 0.3;
+   // static final double VFB_INTAKE_POS = 0;
+
+  //  RobotHardware robot = new RobotHardware(this);
+    //PIDController ridicareController = new PIDController(0.004, 0.008, 1, this);
+    //double ridicareTarget = 1;
+
 
     @Override
     public void init() {
         robot.init();
     }
 
-    private void ridicare() {
+    public void ridicare() {
         if (gamepad2.dpad_down) {
             ridicareTarget = 0;
         } else if (gamepad2.dpad_left) {
@@ -59,16 +64,30 @@ public class Prototype extends OpMode {
         robot.vFB2.setPosition(1 - VFB_OUTTAKE_POS);
     }
 
+	private void vFBpos3() {
+		robot.vFB1.setPosition(VFB_POS3);
+		robot.vFB2.setPosition(1 - VFB_POS3);
+	}
+
+	private void vFBpos4() {
+		robot.vFB1.setPosition(VFB_POS4);
+		robot.vFB2.setPosition(1 - VFB_POS4);
+	}
+
     private void virtualFourBar() {
         if (gamepad2.left_bumper) {
             vFBIntake();
         } else if (gamepad2.right_bumper) {
             vFBOuttake();
+        } else if (gamepad2.left_trigger>0) {
+	        vFBpos3();
+        } else if (gamepad2.right_trigger>0) {
+	        vFBpos4();
         }
         telemetry.addLine("Virtual Four Bar");
         telemetry.addData("poz VFb1", robot.vFB1.getPosition());
         telemetry.addData("poz VFb2", robot.vFB1.getPosition());
-
+		telemetry.addData("power motor",robot.ridicare.getPower());
     }
 
     private void multitask() {
@@ -86,28 +105,26 @@ public class Prototype extends OpMode {
         telemetry.update();
     }
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	public void loop() {
 		robot.movement(gamepad1);
-		ridicare();
+		if (gamepad2.dpad_down) {
+			ridicareTarget = 0;
+		} else if (gamepad2.dpad_left) {
+			ridicareTarget = RIDICARE_POS_1;
+		} else if (gamepad2.dpad_up) {
+			ridicareTarget = RIDICARE_POS_2;
+		} else if (gamepad2.dpad_right) {
+			ridicareTarget = RIDICARE_POS_3;
+		}
 		virtualFourBar();
 		if (gamepad2.a) {
 			robot.claw.setPower(0.4);
 		} else if (gamepad2.b) {
 			robot.claw.setPower(-0.4);
 		} else robot.claw.setPower(0);
-=======
-    public void loop() {
-        robot.movement(gamepad1);
-//		ridicare();
-        virtualFourBar();
-        if (gamepad2.a) {
-            robot.claw.setPower(0.4);
-        } else if (gamepad2.b) {
-            robot.claw.setPower(-0.4);
-        } else robot.claw.setPower(0);
-        robot.ridicare.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
->>>>>>> 9340429984637aa233529f867469ddf65201ed7f
+
+
 
         telemetry();
     }
