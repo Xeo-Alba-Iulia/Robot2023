228d3ca164aeeacffe5518972b0240fe9931356a
raresNagy <rares.nagy@gmail.com>
2023-02-13 12:36:31 +0200

    autonomia lu nagy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Medii.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name = "medii", group = "A", preselectTeleOp = "TeleOP")
+public class Medii extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPos = new Pose2d(35, -60.355, Math.toRadians(270));
+        Trajectory inceput = drive.trajectoryBuilder(startPos)
+                .lineToLinearHeading(new Pose2d(35, -35, Math.toRadians(315)))
+                .lineToLinearHeading(new Pose2d(36,-12, 0))
+                .forward(18)
+                .lineToLinearHeading(new Pose2d(36, -12, Math.toRadians(45)))
+                .build();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        drive.followTrajectory(inceput);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Prototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Prototype.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.firstinspires.ftc.teamcode.utilities.PIDController;
 
-@TeleOp(name = "prototype", group = "B")
+@TeleOp(name = "TeleOP", group = "A")
 public class Prototype extends OpMode {
 
 
