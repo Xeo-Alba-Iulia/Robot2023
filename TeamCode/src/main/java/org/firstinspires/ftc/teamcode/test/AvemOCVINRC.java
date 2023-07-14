c1b80f119227eaed0dd97f90a2dec6a6bd8bd820
raresNagy <rares.nagy@gmail.com>
2023-07-14 15:27:37 +0300

    reparatii

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/AvemOCVINRC.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AvemOCVINRC extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
